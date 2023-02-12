c0fafcd938ba6060932daf91d3eeceae0dcbd557
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-11 18:51:05 -0600

    Added App Layout using the Plotly Dash library
    The app is composed of a series of drop-down menus, images, buttons, and text elements that allow the user to select various attributes of a UFC fight, such as the weight class, the number of rounds, the type of fight, and the fighters. The "Predict" button is expected to trigger a prediction of the outcome of the fight based on these attributes, and the app is expected to display the prediction as a probability for each fighter.

--- UFC_Prediction_Analyzer/src/app/app.py
+++ UFC_Prediction_Analyzer/src/app/app.py
@@ -102,3 +102,213 @@ def get_fighter_url(fighter):
         return (
             "https://pngimage.net/wp-content/uploads/2018/06/image-not-found-png-9.png"
         )
+
+colors = {"background": "#FAFBFC", "text": "#34495E"}
+
+size = {"font": "20px"}
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.scripts.config.serve_locally = True
+
+app.layout = html.Div(
+    style={"backgroundColor": colors["background"], "height": "650px"},
+    children=[
+        html.H1("UFC Predictions", style={"textAlign": "center"}),
+        html.Div(
+            style={"textAlign": "center"},
+            children=[
+                html.Div(
+                    style={
+                        "width": "30%",
+                        "display": "inline-block",
+                        "textAlign": "center",
+                    },
+                    children=[
+                        html.Label(
+                            "Select Weightclass", style={"fontSize": size["font"]}
+                        ),
+                        dcc.Dropdown(
+                            id="weightclass",
+                            options=[
+                                {"label": wt.upper(), "value": wt}
+                                for wt in df_weight_classes.keys()
+                            ],
+                            value="Lightweight",
+                        ),
+                        html.Br(),
+                        html.Label(
+                            "Number of Rounds", style={"fontSize": size["font"]}
+                        ),
+                        dcc.Dropdown(
+                            id="no_of_rounds",
+                            options=[
+                                {"label": "5 Rounds", "value": 5},
+                                {"label": "3 Rounds", "value": 3},
+                            ],
+                            value=3,
+                        ),
+                        html.Br(),
+                        html.Label("Fight type", style={"fontSize": size["font"]}),
+                        dcc.Dropdown(id="fight_type", value="Non Title"),
+                    ],
+                ),
+                html.Div(
+                    style={"width": "30%", "float": "left", "textAlign": "left"},
+                    children=[
+                        html.Label(
+                            "Red Corner (Favourite)",
+                            style={"textAlign": "center", "fontSize": "35px"},
+                        ),
+                        html.Label("Select Fighter", style={"fontSize": size["font"]}),
+                        dcc.Dropdown(id="red-fighter"),
+                        html.Br(),
+                        html.Center(html.Img(id="red-image", width="100%")),
+                    ],
+                ),
+                html.Div(
+                    style={"width": "30%", "float": "right", "textAlign": "left"},
+                    children=[
+                        html.Label(
+                            "Blue Corner (Underdog)",
+                            style={"textAlign": "center", "fontSize": "35px"},
+                        ),
+                        html.Label("Select Fighter", style={"fontSize": size["font"]}),
+                        dcc.Dropdown(id="blue-fighter"),
+                        html.Br(),
+                        html.Center(html.Img(id="blue-image", width="100%")),
+                    ],
+                ),
+                html.Div(
+                    style={
+                        "width": "40%",
+                        "marginLeft": "auto",
+                        "marginRight": "auto",
+                        "textAlign": "left",
+                    },
+                    children=[
+                        html.Br(),
+                        html.Br(),
+                        html.Center(
+                            html.Button(
+                                "Predict",
+                                id="button",
+                                style={
+                                    "fontSize": "32px",
+                                    "backgroundColor": "rgba(255,255,255,0.8)",
+                                },
+                            )
+                        ),
+                        html.Br(),
+                        html.Div(
+                            style={
+                                "width": "35%",
+                                "float": "left",
+                                "textAlign": "left",
+                                "backgroundColor": "rgba(255,255,255,0.7)",
+                            },
+                            children=[
+                                html.H2(
+                                    "Red-Corner",
+                                    style={
+                                        "textAlign": "center",
+                                        "color": "rgb(102, 0, 0)",
+                                    },
+                                ),
+                                html.H3(
+                                    children=["click \n predict"],
+                                    id="red-proba",
+                                    style={"textAlign": "center"},
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            style={
+                                "width": "35%",
+                                "float": "right",
+                                "textAlign": "left",
+                                "backgroundColor": "rgba(255,255,255,0.7)",
+                            },
+                            children=[
+                                html.H2(
+                                    "Blue-Corner",
+                                    style={
+                                        "textAlign": "center",
+                                        "color": "rgb(0, 51, 102)",
+                                    },
+                                ),
+                                html.H3(
+                                    children=["click \n predict"],
+                                    id="blue-proba",
+                                    style={"textAlign": "center"},
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        ),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Div(
+            [
+                dcc.Markdown(
+                    """
+                #### Web App by [Rajeev Warrier](https://rajeevwarrier.com)
+                **Disclaimer**: I do not condone this app's use for betting. I am not responsible for
+                any damage done or losses incurred by way of this app.
+                """.replace(
+                        "  ", ""
+                    )
+                )
+            ],
+            style={"text-align": "center", "margin-bottom": "15px"},
+        ),
+        html.Div(
+            [
+                dcc.Markdown(
+                    """
+                #### Details
+
+                **AUC score: 72.20%**
+
+                The app does not require any odds. It works purely on stats.
+
+                You can read more on my github [repo](https://github.com/WarrierRajeev/UFC-Predictions)
+
+                You can read more about me and contact me from my [website](https://rajeevwarrier.com/)
+
+
+                ##### Helpful UFC links
+
+                You can find upcoming events [here](https://www.ufc.com/events)
+
+                You can find all the athletes competing [here](https://www.ufc.com/athletes/all)
+
+
+
+                """.replace(
+                        "  ", ""
+                    )
+                )
+            ],
+            style={"text-align": "left", "margin-bottom": "15px"},
+        ),
+        html.Br(),
+        html.Br(),
+    ],
+)
+
+
