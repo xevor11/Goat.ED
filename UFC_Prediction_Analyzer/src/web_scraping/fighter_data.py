728215e58595a4f98895f31b2f980032c13f0bae
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-11 19:14:11 -0600

    One-Hot Encoding list of all fighters

--- UFC_Prediction_Analyzer/src/web_scraping/fighter_data.py
+++ UFC_Prediction_Analyzer/src/web_scraping/fighter_data.py
@@ -15,3 +15,19 @@ class FighterDetailProcessor:
         self._convert_weight_to_pounds()
         self.frame = self._merge_frames()
         self._rename_columns()
+        
+   
+    def _one_hot_encode_win(self):
+
+        self.fights = pd.concat(
+            [self.fights, pd.get_dummies(self.fights["win_by"], prefix="win_by")],
+            axis=1,
+        )
+        self.fights.drop(["win_by"], axis=1, inplace=True)
+
+    def _get_fighters(self):
+
+        red_fighters = self.fights["R_fighter"].value_counts().index
+        blue_fighters = self.fights["B_fighter"].value_counts().index
+
+        return list(set(red_fighters) | set(blue_fighters))
